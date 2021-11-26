5e82d8e5ae512ef304c69f661ffe332196d7bec5
dyha0 <dyha0@DESKTOP-NE2M60M>
2021-11-26 09:06:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//17 практическая 1 задание
+if (File.Exists("input.txt"))
+{
+    Console.WriteLine("Введите слово для поиска");
+    string wordToFind = Console.ReadLine();
+    int count = 0;
+    string punctuation = "«.,!<>{}[]:;?-»";
+    string[] text = File.ReadAllText("input.txt").Split(" ");
+    foreach (char punct in punctuation)
+    {
+        for (int i=0;i<text.Length; i++)
+        {
+            text[i] = text[i].Replace(punct.ToString(), "");
+        }
+    }
+    count = text.Where(i => i.ToLower() == wordToFind.ToLower()).Count();
+    Console.WriteLine($"Слово {wordToFind} встречается {count} раз");
+}
+else
+{
+    Console.WriteLine("Файл с входными данными не найден");
+}
